a6ebc839f3ef91d3fe63e4b1d771a49311acd331
brocg <brock.gion@ndsu.edu>
2022-05-05 23:59:43 -0500

    do final code check, clean up whitespace

--- Home Assignment - 4.py
+++ Home Assignment - 4.py
@@ -7,8 +7,8 @@
 #################################################################
 
 ###############
-# ## Problem 1 ## - COMPLETE
-# ###############
+### Problem 1 ## - COMPLETE
+################
 
 # # Given the string:
 # s = 'django'
@@ -40,7 +40,7 @@
 # # Given this nested list:
 # l = [3, 7, [1, 4, 'hello']]
 # # Reassign "hello" to be "goodbye"
-# l[2][2] = 'goodbye' #list items are indexed, so here you can manually replace. Another approach could be looping through list
+# l[2][2] = 'goodbye' #list items are indexed, so just manually replacing. Another approach could be looping through list
 # print(l)
 
 ###############
@@ -70,9 +70,9 @@
 #     print(uniquevalue)
 
 
-# ###############
-# ## Problem 5 ## - COMPLETE
-# ###############
+################
+### Problem 5 ## - COMPLETE
+################
 
 # # You are given two variables:
 # age = 4
@@ -84,7 +84,7 @@
 # print(f"Hello my dog's name is {name} and he is {age} years old")
 
 
-# Complete the tasks below by writing functions! 
+# # Complete the tasks below by writing functions! 
 
 #####################
 ## -- PROBLEM 6 -- ## - COMPLETE
@@ -94,101 +94,88 @@
 # appears in the list somewhere.
 
 # def arrayCheck(nums):
-#     # CODE GOES HERE
-#     return str([1, 2, 3])[1:-1] in str(nums) #explicity typecasting both to strings, so comparing the sequence of '1,2,3' to the list '1,1,2,3,1', then returns T/F boolean
+#   return str([1, 2, 3])[1:-1] in str(nums) #explicity typecasting both to strings, so comparing the sequence of '1,2,3' to the list '1,1,2,3,1', then returns T/F boolean
 
-# For example:
+## For example:
 # print(arrayCheck([1, 1, 2, 3, 1])) # → True
 # print(arrayCheck([1, 1, 2, 4, 1])) # → False
 # print(arrayCheck([1, 1, 2, 1, 2, 3])) # → True
 
 
-    #####################
-    ## -- PROBLEM 7 -- ## - COMPLETE
-    #####################
+#####################
+## -- PROBLEM 7 -- ## - COMPLETE
+#####################
 
-    # Given a string, return a new string made of every other character starting
-    # with the first, so "Hello" yields "Hlo".
-
+# Given a string, return a new string made of every other character starting
+# with the first, so "Hello" yields "Hlo".
 
 # def stringBits(str):
-#   # CODE GOES HERE
-#   slice notation in python has the syntax [<start>:<stop>:<step>] 
-#   [::2] takes in the full string printing every other character
-#   return print(str[::2]) 
+#   return print(str[::2]) #slice notation in python has the syntax [<start>:<stop>:<step>], [::2] takes in the full string printing every other character
 
 # # For example:
 # stringBits('Hello') #→ 'Hlo'
 # stringBits('Hi') #→ 'H'
 # stringBits('Heeololeo') #→ 'Hello'
 
-    #####################
-    ## -- PROBLEM 8 -- ## - COMPLETE
-    #####################
+#####################
+## -- PROBLEM 8 -- ## - COMPLETE
+#####################
 
-    # Given two strings, return True if either of the strings appears at the very end
-    # of the other string, ignoring upper/lower case differences (in other words, the
-    # computation should not be "case sensitive").
-    #
-    # Note: s.lower() returns the lowercase version of a string.
-    #
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
 
 # def end_other(a, b):
-
 #     a1 = a.lower()
 #     b1 = b.lower()
- 
 #     if a1.endswith(b1) or b1.endswith(a1):
 #         return True
 #     else:
 #         return False
 
-# # Examples:
+## Examples:
 # print(end_other('Hiabc', 'abc')) #→ True
 # print(end_other('AbC', 'HiaBc')) #→ True
 # print(end_other('abc', 'abXabc')) #→ True
 
 
-    #####################
-    ## -- PROBLEM 9 -- ## - COMPLETE
-    #####################
+#####################
+## -- PROBLEM 9 -- ## - COMPLETE
+#####################
 
-    # Given a string, return a string where for every char in the original,
-    # there are two chars.
+# Given a string, return a string where for every char in the original,
+# there are two chars.
 
-    # doubleChar('The') → 'TThhee'
-    # doubleChar('AAbb') → 'AAAAbbbb'
-    # doubleChar('Hi-There') → 'HHii--TThheerree'
-    
 # def doubleChar(str):
 #   newStr = ''
 #   for x in str:
 #     newStr += x + x
 #   return newStr
-  
-# print(doubleChar("brock"))
+
+## Examples
+# print(doubleChar('The')) #→ 'TThhee'
+# print(doubleChar('AAbb')) #→ 'AAAAbbbb'
+# print(doubleChar('Hi-There')) #→ 'HHii--TThheerree'
 
 
-    #####################
-    ## -- PROBLEM 10 -- ## - COMPLETE
-    #####################
+#####################
+## -- PROBLEM 10 -- ## - COMPLETE
+#####################
 
-    # Read this problem statement carefully!
+# Read this problem statement carefully!
 
-    # Given 3 int values, a b c, return their sum. However, if any of the values is a
-    # teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
-    # and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
-    # takes in an int value and returns that value fixed for the teen rule.
-    #
-    # In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
-    # Define the helper below and at the same indent level as the main no_teen_sum().
-    # Again, you will have two functions for this problem!
-    #
-    # Examples:
-    #
-    # no_teen_sum(1, 2, 3) → 6
-    # no_teen_sum(2, 13, 1) → 3
-    # no_teen_sum(2, 1, 14) → 3
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
 
 # def no_teen_sum(a, b, c):
 #   # CODE GOES HERE
@@ -198,18 +185,21 @@
 #   # CODE GOES HERE
 #   if n == 15 or n == 16:
 #     return n
-#   return 0
+#   elif n == 13 or n == 14 or n == 17 or n == 18 or n == 19:
+#     return 0
+#   else:
+#    return n
 
-# print(no_teen_sum(15,15,16))
+# print(no_teen_sum(15, 2, 3)) #→ 6
+# print(no_teen_sum(2, 13, 1)) #→ 3
+# print(no_teen_sum(2, 1, 14)) #→ 3
 
+#####################
+## -- PROBLEM 11 -- ## - COMPLETE
+#####################
 
-
-    #####################
-    ## -- PROBLEM 11 -- ## - COMPLETE
-    #####################
-
-    # Return the number of even integers in the given array.
-    #
+# Return the number of even integers in the given array.
+#
 
 # def count_evens(nums):
 #   # CODE GOES HERE
@@ -224,5 +214,3 @@
 # print(count_evens([2, 1, 2, 3, 4])) #→ 3
 # print(count_evens([2, 2, 0])) #→ 3
 # print(count_evens([1, 3, 5])) #→ 0
-
-
